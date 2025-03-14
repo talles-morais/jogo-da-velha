a73000fae6e0a8e8e8c20192ce5761ea0f208972
talles-morais <talves2503@gmail.com>
2025-03-14 15:25:39 -0300

    (#2)feat: show errors and change players

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ var vencedor = ' '
 var fimJogo = false
 var tab = []
 
+function trocarJogador(jog) {
+    if (jog === 'o')
+        jogador = 'x'
+    if (jog === 'x')
+        jogador = 'o'
+}
+
 // a
 for (let i = 0; i < 3; i++) {
     tab[i] = []
@@ -21,11 +28,32 @@ while ((jogadas < 9) && (fimJogo === false)) {
         for (let j = 0; j < 3; j++) {
             process.stdout.write(`[${tab[i][j]}]`);
         }
-        process.stdout.write("\n")      
+        process.stdout.write("\n")
     }
     lin = parseInt(prompt(`${jogador} em [linha]:`))
     col = parseInt(prompt(`${jogador} em [coluna]:`))
     // c
+    if (lin < 0 || lin > 2 || col < 0 || col > 2) {
+        console.log("Jogada inválida, fora do tabuleiro");
+    }
+
+    if (tab[lin][col] !== ' ') {
+        console.log("Campo preenchido, escolha outro");
+    }
+
+    tab[lin][col] = jogador
+    jogadas++;
+
+    if (
+        tab[0] == ["o", "o", "o"] ||
+        tab[1] == ["o", "o", "o"] ||
+        tab[2] == ["o", "o", "o"] 
+    ) {
+        vencedor = jogador
+        break;
+    } else {
+        trocarJogador(jogador)
+    }
 }
 
 // d
