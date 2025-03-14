74590ff194ef3054571846f77e241b46e8cf57c2
talles-morais <talves2503@gmail.com>
2025-03-14 15:01:27 -0300

    fix: file name

--- main.js
+++ main.js
@@ -5,21 +5,20 @@ var jogadas = 0;
 var jogador = 'o'
 var vencedor = ' '
 var fimJogo = false
-var tab = [3]
+var tab = []
 
 // a
-for (let i = 0; i < tab.length; i++) {
-    tab[i] = [' ', ' ', ' ']
+for (let i = 0; i < 3; i++) {
+    tab[i] = []
+    for (let j = 0; j < 3; j++) {
+        tab[i][j] = ' '
+    }
 }
 
 while ((jogadas < 9) && (fimJogo === false)) {
     // b
-    tab.forEach((elem) => {
-        console.log(`[${elem}]`)
-    })
     lin = parseInt(prompt(`${jogador} em [linha]:`))
     col = parseInt(prompt(`${jogador} em [coluna]:`))
-    jogadas++;
     // c
 }
 
